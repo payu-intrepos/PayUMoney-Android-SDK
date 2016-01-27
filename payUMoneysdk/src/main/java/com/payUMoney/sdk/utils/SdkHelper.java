2800cdf91599d7684a63e5f57a47123d072afaaf
Sagar Singh Chauhan <sagar.chauhan@payu.in>
2016-01-27 16:46:57 +0530

    changed checkNetworkInfo to support tabs as well

--- payUMoneysdk/src/main/java/com/payUMoney/sdk/utils/SdkHelper.java
+++ payUMoneysdk/src/main/java/com/payUMoney/sdk/utils/SdkHelper.java
@@ -37,22 +37,17 @@ public class SdkHelper {
     public static boolean checkNetwork(Context c) {
         ConnectivityManager conMgr = (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        if ( conMgr != null && conMgr.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTED
-                || conMgr.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTING
-                || conMgr.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTED
-                || conMgr.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTING ) {
-
-            return true;
-            // notify user you are online
+        if(conMgr != null){
+            NetworkInfo resultTypeMobile = conMgr.getNetworkInfo(0);
+            NetworkInfo resultTypeWifi = conMgr.getNetworkInfo(1);
+            if(((resultTypeMobile != null && resultTypeMobile.isConnectedOrConnecting())) || (resultTypeWifi != null && resultTypeWifi.isConnectedOrConnecting())){
+                return true;
+            }
+            else
+                return false;
 
         }
-        else if ( conMgr != null && conMgr.getNetworkInfo(0).getState() == NetworkInfo.State.DISCONNECTED
-                && conMgr.getNetworkInfo(1).getState() == NetworkInfo.State.DISCONNECTED) {
 
-
-            return false;
-            // notify user you are not online
-        }
         else {
             return false;
         }
